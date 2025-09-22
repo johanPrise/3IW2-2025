c4a7c4e19480309d53fa5ccfae29022d889150e2
Yves Skrzypczyk <ysk@wizards-technologies.com>
2025-09-22 14:12:34 +0200

    init docker

---
+++ www/index.php
@@ -0,0 +1,20 @@
+<?php
+// Commentaire sur une seule ligne
+
+/*
+    Plusieurs lignes
+*/
+
+// Les variables
+//Convention de nommage : camelCase (PascalCase, snake_case, kebab-case)
+//Anglais
+//Cohérence
+$myFirstname;
+
+//Déclaration dynamique
+//Typage dynamique
+//Types : String, Int, Bool, Float, Null
+$myFirstname = "Yves";
+$myFirstname = 12;
+
+
